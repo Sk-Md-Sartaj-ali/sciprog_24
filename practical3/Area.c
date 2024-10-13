10b94e632ab1e35d4d526998dbf291e1dec357b6
Sartaj Ali <shaikmohammadsartajali@gmail.com>
2024-10-13 02:29:44 +0100

    Modifying the Area.c code

--- practical3/Area.c
+++ practical3/Area.c
@@ -2,19 +2,24 @@
 #include<math.h>
 
 int main(void) {
-	
-	float a,b,sum;
-	a=0;
-	b=M_PI*60/(float)180;
-	int i,N=12;
-	
-	for (i=5;i<60;i+=5) {
-		sum+=2*tan(M_PI*(float)i/180.0);
+
+   /* Declaring the variables */    
+	float a, b, sum;
+	int i,N;
+   /* Initialising the variables*/	
+	a = 0;
+	b = M_PI * 60 / (float)180;
+        N=12;
+   /* writing the main code*/
+	for (i = 5; i < 60; i += 5)
+	{
+		sum += 2 * tan(M_PI * (float)i / 180.0);
 	}
-	sum+=tan(M_PI*60.0/(float)180);
-	sum*=(b-a)/(2*(float)N);
-	printf("The computed value is %f\n",sum);
-	printf("The actual value is %f\n",logf(2));
-	printf("The difference is %f\n",fabs(sum-logf(2)));
+	sum += tan(M_PI * 60.0 / (float)180);
+	sum *= (b - a) / (2 * (float)N);
+   /* Printing the output */	
+	printf("The computed value using the trapezoidal rule is %f\n", sum);
+	printf("The actual value using logf(2) is %f\n", logf(2));
+	printf("The difference is %f\n", fabs(sum - logf(2)));
 	return(0);
 }
